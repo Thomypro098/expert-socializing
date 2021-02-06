b3096fdedf93b9828893621e4dc2a36db371fd19
xXRadPlayzXx <sub2radplayz@gmail.com>
2021-02-06 10:56:04 +0000

    t

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,38 @@
 import { Command } from '../utils/types';
 
+import ms from 'pretty-ms';
+
 const PingCommand: Command = {
 	name: 'ping',
 
 	callback: async (message, args, bot) => {
-		await message.reply('yo');
+		const msg = await bot.sendEmbed({ title: 'Pinging...' }, message);
+
+		const uptime = ms(Math.floor(bot.uptime!));
+
+		const embed = await bot.createEmbed(
+			{
+				title: 'Pong :ping_pong: ',
+				fields: [
+					{
+						name: 'API Latency:',
+						value: `**\`\`${bot.ws.ping}\`\`**ms`
+					},
+					{
+						name: 'Bot Latency:',
+						value: `**\`\`${
+							msg.createdTimestamp - message.createdTimestamp 
+						}\`\`**ms`
+					},
+					{
+						name: 'UPTime:',
+						value: `**\`\`${uptime}\`\`**`
+					}
+				]
+			},
+			message
+		);
+		await msg.edit(embed)
 	}
 };
 
