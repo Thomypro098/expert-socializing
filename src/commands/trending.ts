c4be4214cd39af2e6b081dae511927d6d7450791
xXRadPlayzXx <sub2radplayz@gmail.com>
2021-02-06 16:45:57 +0000

    t

--- src/commands/trending.ts
+++ src/commands/trending.ts
@@ -1,52 +1,46 @@
 import { Command } from '../utils/types';
-import { MessageEmbed } from 'discord.js'
-const YTS = require('yt-trending-scraper');
-
+import { MessageEmbed } from 'discord.js';
+// @ts-ignore
+import YTS from 'yt-trending-scraper';
 const YTrendingCommand: Command = {
-    name: 'trending',
+	name: 'trending',
 
-    callback: async (message, args, bot) => {
-        const eEmbed = new MessageEmbed()
-            .setTitle('No Arguments Defined')
-            .setAuthor(
-                message.guild?.name,
-                bot.user?.displayAvatarURL()
-            )
-            .setDescription('Please use ISO2 country codes.\nYou may find the list [here](http://www.fao.org/countryprofiles/iso3list/en/)')
-            .setFooter(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
-            .setTimestamp()
+	callback: async (message, args, bot) => {
+		const eEmbed = await bot.createEmbed(
+			{
+				title: `No Arguments Defined`,
+				description: `Please use ISO2 country codes.\nYou may find the list [here](http://www.fao.org/countryprofiles/iso3list/en/)`
+			},
+			message
+		);
 
-        let i = 1;
+		let i = 1;
 
-        if (!args[0]) return message.channel.send(eEmbed)
+		if (!args[0]) return message.channel.send(eEmbed);
 
-        const embed = new MessageEmbed()
-            .setTitle(`Top 3 ${args[0].toUpperCase()} Youtube Trendings`)
-            .setAuthor(
-                message.guild?.name,
-                bot.user?.displayAvatarURL()
-            )
-            .setFooter(
-                message.author.username,
-                message.author.displayAvatarURL({ dynamic: true })
-            )
+		const embed = await bot.createEmbed(
+			{
+				title: `Top 3 Trending Videos On ${args[0].toUpperCase()}`
+			},
+			message
+		);
 
-        YTS.scrape_trending_page(args[0].toUpperCase(), true).then(async (data: any) => {
-            const videos = data.slice(0, 3)
-            for (const vid of videos) {
-                embed.addFields(
-                    [
-                        {
-                            name: `Video #${i++}`,
-                            value: `**Title:** ${vid.title}\n**Author:** [${vid.author}](https://www.youtube.com/channel/${vid.authorId})\n**Video:** [Video Link](https://www.youtube.com/watch?v=${vid.videoId})\n**Views:** ${vid.viewCount}\n`,
-                        }
-                    ]
-                )
-            }
-            
-            message.channel.send(embed);
-        }).catch((err: any) => console.error(err))
-    }
-}
+		YTS.scrape_trending_page(args[0].toUpperCase(), true)
+			.then(async (data: any) => {
+				const videos = data.slice(0, 3);
+				for (const vid of videos) {
+					embed.addFields([
+						{
+							name: `Video #${i++}`,
+							value: `**Title:** ${vid.title}\n**Author:** [${vid.author}](https://www.youtube.com/channel/${vid.authorId})\n**Video:** [Video Link](https://www.youtube.com/watch?v=${vid.videoId})\n**Views:** ${vid.viewCount}\n`
+						}
+					]);
+				}
+
+				message.channel.send(embed);
+			})
+			.catch((err: any) => console.error(err));
+	}
+};
 
 export default YTrendingCommand;
