c7ded352748b6d7582f6ffe1611e697fa047a8f2
Thomypro098 <68909952+Thomypro098@users.noreply.github.com>
2021-02-06 17:52:22 +0100

    Bot Updates

--- src/events/Client Events/ready.ts
+++ src/events/Client Events/ready.ts
@@ -3,6 +3,9 @@ import { Event } from '../../utils/types';
 import chalk = require ('chalk');
 
 const readyEvent: Event = (client) => {
+	client.user?.setActivity('over social media', {
+		type: 'WATCHING'
+	})
 	client.logger.success(
 		`Successfully logged in as ${chalk.cyanBright(`${client.user!.username}`)}`
 	);
